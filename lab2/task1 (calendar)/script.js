66da58684e89a1e7add7c9bff2cea51a822b0155
3emlekop <vladtk.fm@gmail.com>
2024-02-25 22:49:40 +0200

    Rewritten first two tasks using script.js to generate cells

--- lab2/task1 (calendar)/script.js
+++ lab2/task1 (calendar)/script.js
@@ -1,24 +1,39 @@
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const table = document.getElementById("calendar");
 const dayNameClass = "dayNameElement";
 const workDayClass = "workDayElement";
 const weekendClass = "weekendElement";
-const calendarElement = document.getElementsByClassName("calendarElement");
+
+const daysCount = 31;
 
 function createCalendar() {
+    let counter = 1;
     for(i = 0; i < 6; i++)
     {
+        const row = table.insertRow(i);
+
         for(j = 0; j < 7; j++)
         {
-            if(i == 0)
-                createCalendarElement(dayNameColor);
+            if(i == 0) {
+                createCalendarElement(row, dayNameClass, days[j]);
+                continue;
+            }
 
-            if(j > 5)
-                createCalendarElement(workDayColor);
+            if(counter > daysCount)
+                break;
+
+            createCalendarElement(row, j < 5 ? workDayClass : weekendClass, counter);
+            counter++;
         }
     }
 }
 
-function createCalendarElement() {
-    //create element with appropriate class
+function createCalendarElement(row, className, text) {
+    const cell = row.insertCell();
+    const element = document.createElement("div");
+    element.className = className;
+    element.textContent = text;
+    cell.appendChild(element);
 }
 
 createCalendar();
---
+++ lab2/task2 (ships)/script.js
@@ -0,0 +1,34 @@
+const positions =
+    [
+        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    ];
+const table = document.getElementById("battlefield");
+const cellClass = "cell";
+const shipClass = "ship";
+
+function createBattlefield() {
+    for (i = 0; i < 10; i++) {
+        const row = table.insertRow(i);
+        for (j = 0; j < 10; j++) {
+            createCalendarElement(row, positions[i][j] == 0 ? cellClass : shipClass);
+        }
+    }
+}
+
+function createCalendarElement(row, className) {
+    const cell = row.insertCell();
+    const element = document.createElement("div");
+    element.className = className;
+    cell.appendChild(element);
+}
+
+createBattlefield();
